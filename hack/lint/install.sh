47320c69d6c5ad41cfae341c70c9d00a3e45aacb
Liran BG <liranbg@users.noreply.github.com>
2024-09-01 23:03:47 +0300

    [Security] Bump dependencies (#24)

--- hack/lint/install.sh
+++ hack/lint/install.sh
@@ -29,18 +29,6 @@ fi
 echo "Creating bin directory: ${BIN_DIR}"
 mkdir -p "${BIN_DIR}"
 
-if [[ ! -f ${BIN_DIR}/impi ]] ; then
-    echo "impi binary does not exist. Fetching and installing..."
-    curl -sSfL --retry 3 https://api.github.com/repos/pavius/impi/releases/latest \
-      | grep -i "browser_download_url.*impi.*${OS_NAME}" \
-      | cut -d : -f 2,3 \
-      | tr -d '"' \
-      | tr -d '[:space:]' \
-      | xargs curl -sSL --output "${BIN_DIR}"/impi
-    chmod +x "${BIN_DIR}/impi"
-    echo "impi installed in: ${BIN_DIR}/impi"
-fi
-
 if [[ $# -ne 0 && "$1" == "force" ]]
   then
     echo "Force install golangci-lint requested"
@@ -49,6 +37,6 @@ fi
 
 if [[ $FORCE_INSTALL = true || ! -f ${BIN_DIR}/golangci-lint ]] ; then
     echo "golangci-lint binary does not exist or force install requested. Fetching and installing..."
-    curl -sSfL --retry 3 https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${BIN_DIR}" v1.50.1
+    curl -sSfL --retry 3 https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${BIN_DIR}" v1.55.1
     echo "golangci-lint installed in: ${BIN_DIR}/golangci-lint"
 fi
